d0264de57d3577abb627819f323fa27545f44031
Tsingyue Zhao <zhaotsingyue@gmail.com>
2021-09-09 01:20:37 +0800

    完善了RegisterActivity.java的基本框架，写明了所有TODO

--- app/src/main/java/com/example/testing/RegisterActivity.java
+++ app/src/main/java/com/example/testing/RegisterActivity.java
@@ -1,17 +1,35 @@
 package com.example.testing;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.IOException;
+
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class RegisterActivity extends AppCompatActivity {
 
     ImageView backButton;
+    Button button;
     EditText username;
     EditText password;
-    String UserID;
+//    String userId;
     MyApplication myapp;
 
     @Override
@@ -20,19 +38,80 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         backButton = findViewById(R.id.BackButton);
+        button = findViewById(R.id.register_confirm);
         username = findViewById(R.id.Userid_edit_text);
         password = findViewById(R.id.Password_edit_text);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                TODO：发送Username和password（已完成，待测试）
+//                TODO：加（二次）验证密码+TODO：加入global variable，记录当前用户的id（未完成！）
+                myapp = (MyApplication) getApplication();
+
+                String userId = username.getText().toString();
+                String passwordText = password.getText().toString();
+                // for debug
+                System.err.println("before post: here!");
+
+                OkHttpClient client = new OkHttpClient();
+                FormBody body = new FormBody
+                        .Builder()
+                        .add("userId", userId)
+                        .add("password", passwordText)
+                        .build();
+                Request request = new Request
+                        .Builder()
+                        .url(myapp.baseUrl + "/register")
+                        .post(body)
+                        .build();
+
+                Call call = client.newCall(request);
+                call.enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        System.err.println("Register: network failed");
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        System.err.println(response.toString());
+                        if(response.isSuccessful()) {
+                            String codeText = response.body().string();
+                            System.err.println("Register codeText: " + codeText);
+                            JsonObject jsonObject = JsonParser.parseString(codeText).getAsJsonObject();
+                            int code = jsonObject.get("code").getAsInt();
+
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    username.getText().clear();
+                                    password.getText().clear();
+                                    if(code == 0) {
+                                        Toast.makeText(getApplicationContext(), "注册成功", Toast.LENGTH_SHORT).show();
+                                        myapp.setUsername(userId);
+                                        Intent intent = new Intent(RegisterActivity.this, HomeActivity.class);
+                                        startActivity(intent);
+                                    } else {
+                                        Toast.makeText(getApplicationContext(), "用户名已存在，注册失败", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                        } else {
+                        }
+                    }
+                });
+
+
+//                finish();
+            }
+        });
 
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                //TODO：发送Username和password
-                //TODO：加验证密码+TODO：加入global variable，记录当前用户的id
-                myapp = (MyApplication) getApplication();
-                UserID = username.getText().toString();
-                myapp.setUsername(UserID);
-                finish();
+                //TODO: 如何从RegisterActivity回到开始的页面
             }
         });
     }
--- app/src/main/java/com/example/testing/adapter/SearchListAdapter.java
+++ app/src/main/java/com/example/testing/adapter/SearchListAdapter.java
@@ -1,84 +1,84 @@
-package com.example.testing.adapter;
-
-import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.TextView;
-
-import com.example.testing.R;
-import com.example.testing.jsonTool.SearchListEntity;
-
-import java.util.ArrayList;
-
-public class SearchListAdapter extends BaseAdapter {
-
-    private ArrayList<SearchListEntity> Data;
-    private Context Context;
-    private int mCurrentItem = 0;
-    private boolean isClick = false;
-
-    public SearchListAdapter(Context sContext, ArrayList<SearchListEntity> ssData, yout) {
-        this.Data = ssData;
-        this.Context = sContext;
-    }
-
-    public SearchListAdapter() {
-    }
-
-    @Override
-
-    public int getCount() {
-        return Data.size();
-    }
-
-    @Override
-    public Object getItem(int position) {
-        return position;
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        if (convertView == null) {
-            convertView = LayoutInflater.from(Context).inflate(R.layout.sj_single, parent, false);
-        }
-        TextView sTime = (TextView) convertView.findViewById(R.id.stime);
-        TextView sName = (TextView) convertView.findViewById(R.id.name);
-        TextView sNumber = (TextView) convertView.findViewById(R.id.number);
-
-        sTime.setText(ssData.get(position).getTime());
-        sName.setText(ssData.get(position).getName());
-        sNumber.setText(ssData.get(position).getNumber());
-
-        /*
-         *这个进行判断mCurrentItem=行号，isClick是否点击
-         */
-        if (mCurrentItem == position && isClick) {
-            convertView.setBackgroundColor(Color.parseColor("#FFEFDB"));
-            sTime.setTextColor(Color.parseColor("#9933cc"));
-        } else {
-            convertView.setBackgroundColor(Color.parseColor("#ffffff"));
-            sTime.setTextColor(Color.parseColor("#000000"));
-        }
-
-
-        return convertView;
-    }
-
-    //获取行号
-    public void setCurrentItem(int currentItem) {
-        this.mCurrentItem = currentItem;
-    }
-
-    //是否点击
-    public void setClick(boolean click) {
-        this.isClick = click;
-    }
-
-}
+//package com.example.testing.adapter;
+//
+//import android.content.Context;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.BaseAdapter;
+//import android.widget.TextView;
+//
+//import com.example.testing.R;
+//import com.example.testing.jsonTool.SearchListEntity;
+//
+//import java.util.ArrayList;
+//
+//public class SearchListAdapter extends BaseAdapter {
+//
+//    private ArrayList<SearchListEntity> Data;
+//    private Context Context;
+//    private int mCurrentItem = 0;
+//    private boolean isClick = false;
+//
+//    public SearchListAdapter(Context sContext, ArrayList<SearchListEntity> ssData, yout) {
+//        this.Data = ssData;
+//        this.Context = sContext;
+//    }
+//
+//    public SearchListAdapter() {
+//    }
+//
+//    @Override
+//
+//    public int getCount() {
+//        return Data.size();
+//    }
+//
+//    @Override
+//    public Object getItem(int position) {
+//        return position;
+//    }
+//
+//    @Override
+//    public long getItemId(int position) {
+//        return position;
+//    }
+//
+//    @Override
+//    public View getView(int position, View convertView, ViewGroup parent) {
+//        if (convertView == null) {
+//            convertView = LayoutInflater.from(Context).inflate(R.layout.sj_single, parent, false);
+//        }
+//        TextView sTime = (TextView) convertView.findViewById(R.id.stime);
+//        TextView sName = (TextView) convertView.findViewById(R.id.name);
+//        TextView sNumber = (TextView) convertView.findViewById(R.id.number);
+//
+//        sTime.setText(ssData.get(position).getTime());
+//        sName.setText(ssData.get(position).getName());
+//        sNumber.setText(ssData.get(position).getNumber());
+//
+//        /*
+//         *这个进行判断mCurrentItem=行号，isClick是否点击
+//         */
+//        if (mCurrentItem == position && isClick) {
+//            convertView.setBackgroundColor(Color.parseColor("#FFEFDB"));
+//            sTime.setTextColor(Color.parseColor("#9933cc"));
+//        } else {
+//            convertView.setBackgroundColor(Color.parseColor("#ffffff"));
+//            sTime.setTextColor(Color.parseColor("#000000"));
+//        }
+//
+//
+//        return convertView;
+//    }
+//
+//    //获取行号
+//    public void setCurrentItem(int currentItem) {
+//        this.mCurrentItem = currentItem;
+//    }
+//
+//    //是否点击
+//    public void setClick(boolean click) {
+//        this.isClick = click;
+//    }
+//
+//}
